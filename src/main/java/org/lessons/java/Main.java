f1b3bf80fd7145799f22d18ef335b3b907c006d2
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-05-22 15:11:26 +0200

    refactor: previous checks now done via makeFly and makeSwim methods

--- src/main/java/org/lessons/java/Main.java
+++ src/main/java/org/lessons/java/Main.java
@@ -1,24 +1,49 @@
 package org.lessons.java;
 
 public class Main {
+    public static Dog dog = new Dog("Chip");
+
+        
     public static void main(String[] args) {
-        Dog dog = new Dog("Chip");
 
         dog.emitSound();
         System.out.println("(Hi I'm " + dog.getName() + "!)");
         dog.emitSound();
         dog.eat();
         
-        if (dog instanceof Fly) {
-            dog.emitSound();
-            ((Fly) dog).fly();
-        } else if (dog instanceof Swim){
-            dog.emitSound();
-            ((Swim) dog).swim();
-        } else{
-            dog.emitSound();
-            System.out.println("(I can't do none of those actions!)");
-        }
+        // if (dog instanceof Fly) {
+        //     dog.emitSound();
+        //     ((Fly) dog).fly();
+        // } else if (dog instanceof Swim){
+        //     dog.emitSound();
+        //     ((Swim) dog).swim();
+        // } else{
+        //     dog.emitSound();
+        //     System.out.println("(I can't do none of those actions!)");
+        // }
+
+        makeFly(dog);
+        makeSwim(dog);
         
+    }
+
+    public static void makeFly(Animal animal){
+        if(animal instanceof Fly){
+            animal.emitSound();
+            ((Fly) animal).fly();
+        } else {
+            animal.emitSound();
+            System.out.println("(I can't fly!)");
+        }
+    }
+
+    public static void makeSwim(Animal animal){
+        if(animal instanceof Swim){
+            animal.emitSound();
+            ((Swim) animal).swim();
+        } else {
+            animal.emitSound();
+            System.out.println("(I can't swim!)");
+        }
     }
 }
